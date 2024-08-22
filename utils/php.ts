4f12312fa48f17c63770582889e538a9eb1bb7aa
Anton Cherginets <anton.cherginets@gmail.com>
2024-08-22 16:47:47 +0300

    prettier

--- utils/php.ts
+++ utils/php.ts
@@ -1,16 +1,16 @@
 export function number_format(
   number: number,
   decimals: number = 0,
-  dec_point: string = '.',
-  thousands_sep: string = ' '
+  dec_point: string = ".",
+  thousands_sep: string = " "
 ): string {
   // Validate the input number
   if (isNaN(number) || !isFinite(number)) {
-    throw new Error('Input is not a valid number');
+    throw new Error("Input is not a valid number");
   }
 
   // Check if the input number has decimals
-  let [integerPart, decimalPart] = number.toFixed(decimals).split('.');
+  let [integerPart, decimalPart] = number.toFixed(decimals).split(".");
 
   // Add thousands separator to the integer part
   integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
