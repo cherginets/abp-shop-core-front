01b32c604233afd19aefaa17087a00b8d241e29e
Anton Cherginets <anton.cherginets@gmail.com>
2024-08-30 11:04:44 +0300

    constants

--- utils/formatters.ts
+++ utils/formatters.ts
@@ -1,3 +1,10 @@
+import {number_format} from "@/core/utils/php";
+
 export const MOMENT_FOR_FILENAME = "YYYYMMDDHHmmss";
 
 export const MOMENT_DATE_MYSQL = "YYYY-MM-DD HH:mm:ss";
+
+export const formatMoney = (value: string | number) => {
+    if(!value) value = 0;
+    return `${number_format(typeof value === 'string' ? parseFloat(value) : value, 2, '.', ' ')} руб.`;
+}
