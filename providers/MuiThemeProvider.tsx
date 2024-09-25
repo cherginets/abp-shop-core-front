6dd64868e467b34967933b1f2c329e61d1a26c78
Anton Cherginets <anton.cherginets@gmail.com>
2024-09-25 10:39:54 +0300

    lk orders responsive table

--- providers/MuiThemeProvider.tsx
+++ providers/MuiThemeProvider.tsx
@@ -3,11 +3,26 @@ import {ReactNode} from "react";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 import {ThemeProvider} from "@mui/material";
 import theme from "@/styles/theme";
+import {ConfirmProvider} from "material-ui-confirm";
 
 export default function MuiThemeProvider({children}: { children: ReactNode }) {
+
     return <AppRouterCacheProvider>
         <ThemeProvider theme={theme}>
-            {children}
+            <ConfirmProvider defaultOptions={{
+                cancellationText: "Отмена",
+                confirmationText: "Подтвердить",
+                cancellationButtonProps: {
+                    style: { marginRight: "auto" }
+                },
+                confirmationButtonProps: {
+                    variant: "contained"
+                },
+                title: "Вы уверены?",
+                content: "Необходимо подтвердить действие."
+            }}>
+                {children}
+            </ConfirmProvider>
         </ThemeProvider>
     </AppRouterCacheProvider>
 
